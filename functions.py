a475e9e244c40b2520b25cdb7ff7343f77e1ad1f
m1crocat <m1cro_cat@outlook.com>
2024-07-10 18:33:22 +0500

    4.0.0
    удалено почти все, что было ненужно. только самый сок.

--- functions.py
+++ functions.py
@@ -1,176 +1,17 @@
-current_version = "3.1.7"
-release_type = "stable"
-version_date = "18.01.24"
-config_file = 'config.json'
+current_version = "4.0.0"
+version_date = "10.07.24"
+
 
 from random import *
 from time import *
 from turtle import *
 import os
-import sys
 import base64
-import requests
-import shutil
-import main_beta
-import json
-import traceback
 
-def amogus():#инет
-            speed(15)
-            color1 = (randint(0, 255), randint(0, 255), randint(0, 255))
-            color2 = (randint(0, 255), randint(0, 255), randint(0, 255))
-            pensize(30)
-            fillcolor(color1)
-            begin_fill()
-            right(90)
-            forward(50)
-            right(180)
-            circle(40, -180)
-            right(180)
-            forward(200)
-            right(180)
-            circle(100, -180)
-            backward(20)
-            left(15)
-            circle(500, -20)
-            backward(20)
-            circle(40, -180)
-            left(7)
-            backward(50)
-            up()
-            left(90)
-            forward(10)
-            right(90)
-            down()
-            right(240)
-            circle(50, -70)
-            end_fill()
-            up()
-            right(230)
-            forward(100)
-            left(90)
-            forward(20)
-            right(90)
-            down()
-            fillcolor(color2)
-            begin_fill()
-            right(150)
-            circle(90, -55)
-            right(180)
-            forward(1)
-            right(180)
-            circle(10, -65)
-            right(180)
-            forward(110)
-            right(180)
-            circle(50, -190)
-            right(170)
-            forward(80)
-            right(180)
-            circle(45, -30)
-            end_fill()
-            up()
-            right(60)
-            forward(100)
-            right(90)
-            forward(75)
-            fillcolor(color2)
-            begin_fill()
-            down()
-            forward(30)
-            right(255)
-            circle(300, -30)
-            right(260)
-            forward(30)
-            end_fill()
-def toggleUpdates():#MystieHum\нейросеть
-
-    if os.path.exists(config_file):
-        with open(config_file) as f:
-            config = json.load(f)
-    else:
-        config = {
-            'updates_enabled': True
-        }
-
-    if config['updates_enabled'] == True:
-        update_state = "включена"
-    elif config['updates_enabled'] == False:
-        update_state = "выключена"
-    choice = input(f"1 / 0 - вкл/выкл автоматическую проверку обновлений\nСейчас автопроверка обновлений {update_state}.\nВыберите опцию: ")
-    
-    if choice == '1':
-        print("Обновления включены")
-        config['updates_enabled'] = True
-    elif choice == '0':
-        print("Обновления отключены")
-        config['updates_enabled'] = False
-
-    with open(config_file, 'w') as f:
-        json.dump(config, f)
-
-if __name__ == '__main__':#MystieHum\нейросеть
-    
-    toggleUpdates()
-    
-    # Check config setting
-    if os.path.exists(config_file):
-        with open(config_file) as f:
-            config = json.load(f)
-            if config['updates_enabled']:
-                print("Обновления включены")
-                # call update code
-            else:
-                print("Обновления отключены")
-    else:
-        print("Обновления включены по умолчанию")
-
-def miscMenu():#MystieHum\нейросеть
-	try:
-		while True:
-			choice = int(input("\n1 - проверить обновления\n2 - настроить проверку обновлений\n3 - переключить ветку обновлений\n4 - скачать последную версию\n5 - выйти в главное меню\nВыберите опцию: "))
-			if choice == 1:
-				checkForUpdates()
-			elif choice == 2:
-				toggleUpdates()
-			elif choice == 3:
-				switchBranch()
-			elif choice == 4:
-				dlLastVer()
-			elif choice == 5:
-				break
-			else:
-				print("Такой опции не существует")
-	except ValueError:
-		print("Неправильное значение! Должно быть от 1 до 5!")
-
-def dlLastVer():#MystieHum\нейросеть
-	if os.path.exists(config_file):
-		with open(config_file) as f:
-			config = json.load(f)
-	branch = config['update_branch']
-	version_url = f'https://raw.githubusercontent.com/m1crocat/multipy/{branch}/latest_version.txt'
-	version_response = requests.get(version_url)
-
-	if version_response.status_code == 200:
-		latest_version = version_response.text.strip()
-		zip_url = f'https://github.com/m1crocat/multipy/archive/{branch}.zip'
-		save_file = f'mpy-{latest_version}-{branch}.zip'
-		save_path = os.path.join(os.getcwd(), save_file)
-
-		response = requests.get(zip_url)
-		with open(save_path, 'wb') as f:
-			f.write(response.content)
-
-		print(f"Новая версия сохранена по пути:\n{save_path}")
-		return save_path
-
-	else:
-		print("Произошла ошибка при скачивании. Простите позязя :(")
-		return None
 
 def prt():#я
-    print(" <<MultiPy>> \n 1 - PaintGPT \n 2 - О MultiPy \n 3 - Что нового? \n 4 - Игра КНБ \n 5 - Игра Угадай число \n 6 - Секундомер \n 7 - Таймер обратного отсчета \n 8 - Разное \n 9 - Бросить кубик \n 10 - Погода \n 11 - Генератор \n 12 - Base64 \n 13 - Узнать длину строки (len) \n 14 - Beta \n 15 - Ping \n 16 - Обновления \n 17 - Конвертор(alpha)")
+        print()
+    #
 def convertor():#я
             ipt = input("Введите из чего в что вы хотите перевести \nДоступно: все от мм до км, все от байтов до тб\nНапример: байты - тб, метры - см\n(введите 0 для выхода): ").lower()
             while ipt != "0":
@@ -325,89 +166,6 @@ def convertor():#я
                     per1 = int(input("Введите тб: "))
                     print("Результат:", per1 * 1024)
 
-def updateProgram():#MystieHum\нейросеть
-	if os.path.exists(config_file):
-		with open(config_file) as f:
-			config = json.load(f)
-	branch = config['update_branch']
-	url = f'https://github.com/m1crocat/multipy/archive/{branch}.zip'
-	update_zip = os.path.join('cache', 'update.zip')
-	os.makedirs('cache', exist_ok=True)
-	response = requests.get(url)
-	with open(update_zip, 'wb') as f:
-		f.write(response.content)
-	shutil.unpack_archive(update_zip, 'update')
-	os.remove(update_zip)
-	src_dir = os.path.join('update', f'multipy-{branch}')
-	dest_dir = os.path.dirname(os.path.abspath(__file__))
-	for src_name in os.listdir(src_dir):
-		src_path = os.path.join(src_dir, src_name)
-		dest_path = os.path.join(dest_dir, src_name)
-		if os.path.exists(dest_path):
-			os.remove(dest_path)
-		shutil.move(src_path, dest_dir)
-	shutil.rmtree(os.path.join('update'))
-	shutil.rmtree(os.path.join('cache'))
-	print("Обновление завершено успешно!\n")
-	args = [sys.executable] + sys.argv
-	os.execv(sys.executable, args)
-
-def checkForUpdates():#MystieHum\нейросеть
-	if os.path.exists(config_file):
-		with open(config_file) as f:
-			config = json.load(f)
-	branch = config['update_branch']
-	print("Проверка обновлений.. ")
-	cache_dir = os.path.join('cache')
-	if os.path.isdir(cache_dir):
-		shutil.rmtree(cache_dir)
-	cache_folder = 'cache'
-	if not os.path.exists(cache_folder):
-		os.mkdir(cache_folder)
-	latest_version_url = f'https://raw.githubusercontent.com/m1crocat/multipy/{branch}/latest_version.txt'
-	latest_version_path = os.path.join(cache_folder, 'latest_version.txt')
-	if not os.path.exists(latest_version_path):
-		response = requests.get(latest_version_url)
-		with open(latest_version_path, 'w') as f:
-			f.write(response.text)
-	with open(latest_version_path) as f:
-		latest_version = f.read().strip()
-	if latest_version > current_version:
-		print(f"Доступна новая версия {latest_version} (текущая версия: {current_version})")
-		update = input("Хотите обновиться? (y/n): ")
-		if update.lower() == 'y' or update.lower() == 'д':
-			print("Обновляемся...")
-			updateProgram()
-		elif update.lower() == 'n' or update.lower() == 'н':
-			sleep(0.3)
-			print("Обновление отменено\n")
-			shutil.rmtree(os.path.join('cache'))
-	else:
-		print("Обновлений не найдено!\n")
-		shutil.rmtree(os.path.join('cache'))
-
-def switchBranch():#MystieHum\нейросеть
-	while True:
-		if os.path.exists(config_file):
-			with open(config_file) as f:
-				config = json.load(f)
-		print(f"Сейчас используется ветка {config['update_branch']} для обновлений.\n")
-		print("1 - переключиться на beta")
-		print("2 - переключиться на main")
-		print("3 - в главное меню")
-		choice = input("Выберите опцию: ")
-		if choice == '1':
-			config['update_branch'] = "beta"
-			with open(config_file, 'w') as f:
-				json.dump(config, f)
-		elif choice == '2':
-			config['update_branch'] = "main"
-			with open(config_file, 'w') as f:
-				json.dump(config, f)
-		elif choice == '3':
-			break
-		print(f"Вы переключились на ветку {config['update_branch']}.")
-
 def paintgpt():#полностью я
     try:
         def paint():
@@ -426,8 +184,6 @@ def paintgpt():#полностью я
             penup()
             goto(randint(-250, 250), randint(-250, 250))
             pendown()
-            if ran1 == 1:
-                 amogus()
             if ran1 == 2:
                 # звезда
                 begin_fill()
@@ -535,7 +291,7 @@ def paintgpt():#полностью я
                 print("PaintGPT закрыт")
 
 def info():#я
-    print(f"\nПрограмма MultiPy.\nВерсия {current_version} {release_type} от {version_date}.")
+    print(f"\nПрограмма MultiPy.\nВерсия {current_version} от {version_date}.")
 
 def igraUgadaika():#я
     count1 = 1
@@ -602,51 +358,13 @@ def timer():#инет\нейросеть
     except ValueError:
         print("Неправильное значение!")
 
-def raznoe():#я
-    ipt1 = int(input("1 - Интересный узор\n2 - AMOGUS\n3 - Бенчмарк\nВыберите что-нибудь: "))
-    if ipt1 == 1:
-        try:
-            hideturtle()
-            per2 = 10
-            per1 = 1
-            while True:
-                speed(per2)
-                forward(per1)
-                left(90)
-                per1 += 1
-                per2 += 1
-        except Exception:
-            print("Закрыто")
-    elif ipt1 == 2:
-        amogus()
-    elif ipt1 == 3:
-         bench()
-    else:
-        print("дУ ю СпИк ИнГлИш?!!?")
 def bench():#я(идея моя)
      input("Для начала теста нажмите Enter: ")
      time_st = time()
      sleep(5)
      time_end = time()
      print("Результат теста:", (time_end - time_st) - 5, "сек. время вычислений")
-def weather():# нейросеть
-    print("Команда полностью написана через ChatGPT")
-    city = input("Введите название города: ")
-    api_key = '9f847b92b31f51a681d9792e18973c03'
-    base_url = 'https://api.openweathermap.org/data/2.5/weather'
-    params = {
-        'q': city,
-        'appid': api_key,
-        'units': 'metric'
-    }
-    response = requests.get(base_url, params=params)
-    weather_data = response.json()
-    if weather_data['cod'] == 200:
-        temperature = weather_data['main']['temp']
-        description = weather_data['weather'][0]['description']
-        print(f"Температура в городе {str(city).capitalize()}: {temperature}°C, {description}")
-    else:
-        print("Не удалось получить данные о погоде") 
+
 
 def gen(): #частично я частично инет
     per1 = int(input("1 - генератор букв, 2 - генератор цифр, 3 - генератор пароля(буквы + цифры): "))
@@ -716,66 +434,12 @@ def length():#я
     print("Длина строки:", len(per3))
 
 def ping():#я
-    i1 = input("Введите ip, или доменное имя для пинга (укажите после ip -t если надо пинговать бесконечно): ")
+    i1 = input("Введите ip, или доменное имя для пинга (или укажите параметр -t на windows, если надо пинговать бесконечно): ")
     print(os.system(f"ping {i1}"))
 
 def changelog():#я
     print("\nЛист обновлений!\n")
-    print("3.1.7 - 18.01.24")
-    print("Мини-обнова. MystieHum больше не разработчик программы. ")
-    print("3.1.6 - 09.11.23")
-    print("Обновление, аМоГуС в PaintGPT")
-    print("3.1.5.9 - 09.11.23")
-    print("Починил то что сломал(кошк)")
-    print("3.1.5.8 - 07.11.23")
-    print("Если при проверке обновлений произойдет ошибка, то программа продолжит работу (m1cro_cat)")
-    print("3.1.5.7 - 03.11.23")
-    print("Имя файлов скачанных версий теперь mpy-{версия}-{ветка}.zip (MystieHum)")
-    print("3.1.5.6 - 03.11.23")
-    print("Переименован пункт 'настроить обновления' в 'настроить проверку обновлений', чтобы избежать путаницы.\nТакже немного прибран код (MystieHum)")
-    print("3.1.5.5 - 03.11.23")
-    print("Фикс перевода, фикс даты. (кот)")
-    print("3.1.5.3-4 - 03.11.23")
-    print("Возможность скачать последнюю версию через п.16 (Обновления) (MystieHum & m1cro_cat)")
-    print("3.1.5-3.1.5.2 - 02.11.23")
-    print("Переключение ветки обновлений (MystieHum (он молодец))")
-    print("3.1.4.8 - 02.11.23")
-    print("Важный фикс обновлений (m1cro_cat)")
-    print("3.1.4.6 - 26.08.23")
-    print("Фиск игры Угадай число")
-    print("3.1.4.2-5 - 22-25.08.23")
-    print("Фиксы")
-    print("3.1.4.1 - 22.08.23")
-    print("хот дог фиксы(у байтов отвал(хнык хнык))")
-    print("3.1.4 - 22.08.23")
-    print("Добавление конвертора(п. 17), минификсы")
-    print("3.1.3.1 - 22.08.23")
-    print("Поздравляю свою маму с днем рождения, эта версия для тебя :3")
-    print("3.1.3 - 20.08.23")
-    print("Добавлена настройка автопроверки обновлений (п. 16)\n")
-    print("3.1.1 / 3.1.2 - 20.08.23")
-    print("Перенос всех функций в отдельный файл, крч внутренняя переделка:)\n")
-    print("3.1.0 - 20.08.23")
-    print("Проверка и установка обновлений\nПеределанный таймер и секундомер\nМелкие исправления\n")
-    print("3.0.4 - 18.08.23")
-    print("Фиксы, улучшение кода PaintGPT\n")
-    print("3.0.3 - 17.08.23")
-    print("Много разных фиксов от MystieHum\n")
-    print("3.0.1 / 3.0.2 - 15-16.08.23")
-    print("Минификсы, улучшение PaintGPT, других команд.\n")
-    print("3.0.0 - 11.08.23")
-    print("Полностью переписан интерфейс, улучшен код)\n")
-    print("2.2.1 - 11.08.23")
-    print("Финальная версия 2.x\n")
-    print("2.2.0 - 11.08.23")
-    print("Переименование программы \nОптимизация кода\n")
-    print("2.1.0 - 09.08.23")
-    print("Переделка некоторых функций, и добавление новых\n")
-    print("2.0.0 - 28.07.23")
-    print("Полная переделка кода, убрал лишние функции, которые уже не актуальны. Также добавил кучу всего!\n")
-    print("1.5.0 - 16.07.23")
-    print("Переименовал в projectpb \nДобавил много функций\n")
-
+    print("4.0.0 - 10.07.24")
 
 def knb():#я
     response = int(input("Камень(1), ножницы(2), или бумага?(3) (0 - выход, если вы обиделись введите 4): "))
--- main.py
+++ main.py
@@ -1,58 +1,64 @@
 from functions import *
-try:
-    try:
-        config_file = 'config.json'
-        with open(config_file) as f:
-            config = json.load(f)
-        if config['updates_enabled'] == True:
-            checkForUpdates()
-    except Exception as e:
-        per1 = input("Произошла ошибка:( показать её?").lower
-        if per1 == "y":
-            print(traceback.format_exc())
-    prt()
-    ipt = int(input("Что вы хотите сделать? (Введите 0 или Ctrl+C для выхода): "))
-    while ipt != 0:
-        if ipt == 1:
-            paintgpt()
-        elif ipt == 2:
-            info()
-        elif ipt == 3:
-            changelog()
-        elif ipt == 4:
-            knb() 
-        elif ipt == 5:
-            igraUgadaika()
-        elif ipt == 6:
-            stopwatch()
-        elif ipt == 7:
-            timer()
-        elif ipt == 8:
-            raznoe()
-        elif ipt == 9:
-            print("Вам выпало", randint(1,6))
-        elif ipt == 10:
-            weather()
-        elif ipt == 11:
-            gen()
-        elif ipt == 12:
-            base64fun()
-        elif ipt == 13:
-            length()
-        elif ipt == 14:
-            main_beta.beta()
-        elif ipt == 15:
-            ping()
-        elif ipt == 16:
-            miscMenu()
-        elif ipt == 17:
-            convertor()
-        else: 
-            print("???")
-        ipt1 = input("\nНажмите Enter чтобы показать меню ")
-        if ipt1 == "":
-            prt()
-            ipt = int(input("Что вы хотите сделать? (Введите 0 или Ctrl+C для выхода): "))
-    print("exit...")
-except KeyboardInterrupt:
-    print("\nexit...")
+print(""" 
+ m    m m    m m     mmmmmmm mmmmm  mmmmm m     m
+ ##  ## #    # #        #      #    #   "# "m m" 
+ # ## # #    # #        #      #    #mmm#"  "#"  
+ # "" # #    # #        #      #    #        #   
+ #    # "mmmm" #mmmmm   #    mm#mm  #        #   4.0.0
+""")
+print(" 1 - Утилиты\n 2 - Игры\n 3 - О программе\n 0 - Выход")
+ipt = int(input("Выберите действие: "))
+print("\n")
+while ipt != 0:
+    if ipt == 1:
+        print(" 1 - Секундомер\n 2 - Таймер\n 3 - Генератор\n 4 - Base64\n 5 - Узнать длинну строки\n 6 - Ping\n 7 - Конвертор единиц (beta)\n 8 - Бенчмарк\n 0 - Назад")
+        ipt = int(input("\nВыберите действие: "))
+        while ipt != 0:
+            if ipt == 1:
+                stopwatch()
+            elif ipt == 2:
+                timer()
+            elif ipt == 3:
+                gen()
+            elif ipt == 4:
+                base64fun()
+            elif ipt == 5:
+                length()
+            elif ipt == 6:
+                ping()
+            elif ipt == 7:
+                convertor()
+            elif ipt == 8:
+                bench()
+            elif ipt == 0:
+                break
+            else:
+                print("Введите корректное значение")
+            print(" 1 - Секундомер\n 2 - Таймер\n 3 - Генератор\n 4 - Base64\n 5 - Узнать длинну строки\n 6 - Ping\n 7 - Конвертор единиц (beta)\n 8 - Бенчмарк\n 0 - Назад")
+            ipt = int(input("\nВыберите действие: "))
+    elif ipt == 2:
+        print(" 1 - PrintGPT\n 2 - Игра Камень-Ножницы-Бумага\n 3 - Игра Угадай число\n 0 - Назад")
+        ipt = int(input("\nВыберите действие: "))
+        while ipt != 0:
+            if ipt == 1:
+                paintgpt()
+            elif ipt == 2:
+                knb()
+            elif ipt == 3:
+                igraUgadaika()
+            elif ipt == 0:
+                break
+            else:
+                print("Введите корректное значение")
+            print("\n 1 - PrintGPT\n 2 - Игра Камень-Ножницы-Бумага\n 3 - Игра Угадай число\n 0 - Назад")
+            ipt = int(input("\nВыберите действие: "))
+    elif ipt == 3:
+        info()
+    elif ipt == 0:
+        break
+    else:
+        print("Введите корректное значение")
+    print(" 1 - Утилиты\n 2 - Игры\n 3 - О программе\n 0 - Выход")
+    ipt = int(input("\nВыберите действие: "))
+print("exit...")
+sleep(1)
